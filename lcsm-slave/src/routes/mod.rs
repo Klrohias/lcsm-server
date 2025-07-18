c9546b6f838592599b31e95f8d77bcd5bface2ef
qingyiwebt <qingyiweb@gmail.com>
2025-07-18 19:25:04 +0800

    rename slave api routes

--- lcsm-slave/src/routes/mod.rs
+++ lcsm-slave/src/routes/mod.rs
@@ -7,6 +7,6 @@ mod processes;
 
 pub fn get_routes(state_ref: &AppStateRef) -> Router {
     Router::new()
-        .nest("/instances", instances::get_routes(state_ref))
-        .nest("/processes", processes::get_routes(state_ref))
+        .nest("/instance", instances::get_routes(state_ref))
+        .nest("/process", processes::get_routes(state_ref))
 }
